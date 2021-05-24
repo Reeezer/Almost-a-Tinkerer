b456e7576d2bf272b2d71b7c32dffd01731cb527
Léon Muller <leon.muller@he-arc.ch>
2021-05-24 17:48:05 +0200

    Splashscreen done

--- core/src/ch/hearc/p2/aatinkerer/SplashScreen.java
+++ core/src/ch/hearc/p2/aatinkerer/SplashScreen.java
@@ -3,19 +3,30 @@ package ch.hearc.p2.aatinkerer;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class SplashScreen implements Screen
 {
 	private AATinkererGame game;
-	private Texture background;
+	private Texture background[];
+
 	private int width;
 	private int height;
+
+	private long lastTime;
+	private int frame;
 
 	public SplashScreen(AATinkererGame game)
 	{
 		this.game = game;
-		background = new Texture("background.jpg");
+
+		background = new Texture[] { new Texture("Menus/splash1.png"), new Texture("Menus/splash2.png") };
+		frame = 0;
+		lastTime = TimeUtils.millis();
+
 		width = 0;
 		height = 0;
 	}
@@ -30,12 +41,23 @@ public class SplashScreen implements Screen
 	@Override
 	public void render(float delta)
 	{
+		long firstTime = TimeUtils.millis();
+
+		if (firstTime - lastTime >= 1000) {
+			frame = (frame + 1) % background.length;
+			lastTime = firstTime;
+		}
+
 		if (Gdx.input.isKeyJustPressed(Keys.SPACE))
 			game.toGameScreen();
 
+		Gdx.gl.glClearColor(0, 0.2f, 0.3f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		game.batch.begin();
 
-		game.batch.draw(background, 0, 0, width, height);
+		TextureRegion textureRegion = new TextureRegion(background[frame]);
+		game.batch.draw(textureRegion, 0, 0, (float) width, (float) height, (float) width, (float) height, 1.f, 1.f, (float) 0 * 90.f);
 
 		game.batch.end();
 	}
