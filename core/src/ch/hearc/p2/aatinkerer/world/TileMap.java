d00d321a200f31e29b6d3bb75f2d46b743ef7618
Luca Davide Meyer <luca.meyer@he-arc.ch>
2021-06-11 14:59:40 +0200

    Made it so ressources cannot generate near the hub

--- core/src/ch/hearc/p2/aatinkerer/world/TileMap.java
+++ core/src/ch/hearc/p2/aatinkerer/world/TileMap.java
@@ -70,7 +70,10 @@ public class TileMap
 			// choose a random resource to spawn excluding the first value which is NONE
 			Ressource ressource = Ressource.values()[(random.nextInt(Ressource.values().length) - 1) + 1];
 
-			generate(ressource, life, x, y);
+			// make it so seeds cannot spawn in a way that will make them reach the center (= the hub) so it stays clear
+			if (Math.abs(x - (width / 2)) > (life + 3) || Math.abs(y - (height / 2)) > (life + 3))
+				generate(ressource, life, x, y);
+
 		}
 
 		Hub hub = new Hub(this, width / 2, height / 2);
