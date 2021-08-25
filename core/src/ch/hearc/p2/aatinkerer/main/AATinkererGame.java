614ed11949f91816f49fde2a9a98567a9bbb8116
Léon Muller <leon.muller@he-arc.ch>
2021-08-25 12:18:09 +0200

    Added some sound effect on actions

--- core/src/ch/hearc/p2/aatinkerer/main/AATinkererGame.java
+++ core/src/ch/hearc/p2/aatinkerer/main/AATinkererGame.java
@@ -2,53 +2,45 @@ package ch.hearc.p2.aatinkerer.main;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import ch.hearc.p2.aatinkerer.util.Input;
+import ch.hearc.p2.aatinkerer.util.Sounds;
 
-public class AATinkererGame extends Game
-{
+public class AATinkererGame extends Game {
 	public SpriteBatch batch;
 	public Input input;
 
 	private GameScreen gameScreen;
 	private SplashScreen splashScreen;
-	
-	public Music music;
 
 	@Override
-	public void create()
-	{
+	public void create() {
 		batch = new SpriteBatch();
 
 		input = new Input();
 		Gdx.input.setInputProcessor(input);
-
-		music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
-		music.setVolume(0.1f);
-		music.play();
 		
+		Sounds.MUSIC.setVolume(0.1f);
+		Sounds.MUSIC.play();
+
 		gameScreen = new GameScreen(this);
 		splashScreen = new SplashScreen(this);
 
 		setScreen(splashScreen);
 	}
 
-	public void toGameScreen()
-	{
+	public void toGameScreen() {
 		setScreen(gameScreen);
 	}
 
 	@Override
-	public void render()
-	{
+	public void render() {
 		super.render();
 	}
 
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
 		gameScreen.dispose();
 		batch.dispose();
 	}
