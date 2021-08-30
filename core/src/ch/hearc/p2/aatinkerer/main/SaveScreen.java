e816e0042b8af1be950c50f7c24e27f60c06e6c8
Léon Muller <leon.muller@he-arc.ch>
2021-08-30 11:30:43 +0200

    New menu to choose the save to load, or a new world

---
+++ core/src/ch/hearc/p2/aatinkerer/main/SaveScreen.java
@@ -0,0 +1,133 @@
+package ch.hearc.p2.aatinkerer.main;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class SaveScreen implements Screen
+{
+	private AATinkererGame game;
+
+	private OrthographicCamera camera;
+	private FitViewport viewport;
+
+	private Stage stage;
+
+	public SaveScreen(final AATinkererGame game)
+	{
+		this.game = game;
+
+		this.camera = new OrthographicCamera();
+		this.viewport = new FitViewport(0, 0, camera);
+
+		Table mainTable = new Table();
+		Table buttonsTable = new Table();
+		mainTable.setFillParent(true);
+
+		stage = new Stage();
+		stage.setViewport(viewport);
+		stage.addActor(mainTable);
+
+		// Title
+		LabelStyle labelStyle = new LabelStyle();
+		labelStyle.font = AATinkererGame.font.generateFont(AATinkererGame.titleFontParam);
+		labelStyle.fontColor = AATinkererGame.WHITE;
+
+		Label title = new Label("Worlds", labelStyle);
+
+		// Buttons
+		NinePatch textButtonPatch = new NinePatch(new Texture("Ui/Buttons/textbutton.png"), 2, 2, 2, 2);
+
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.font = AATinkererGame.font.generateFont(AATinkererGame.buttonFontParam);
+		textButtonStyle.overFontColor = AATinkererGame.BLUE;
+		textButtonStyle.fontColor = AATinkererGame.WHITE;
+		textButtonStyle.up = new NinePatchDrawable(textButtonPatch);
+
+		TextButton loadButton = new TextButton("Load world", textButtonStyle);
+		loadButton.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y)
+			{
+				game.toGameScreen();
+				// FIXME disable le bouton si rien n'est sélectionné
+				// FIXME lancer la partie en fonction de ce qui est sélectionné
+			};
+		});
+		buttonsTable.add(loadButton).pad(100);
+
+		TextButton newButton = new TextButton("New world", textButtonStyle);
+		newButton.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y)
+			{
+				game.toDifficultyScreen();
+			};
+		});
+		buttonsTable.add(newButton).pad(100);
+
+		mainTable.add(title);
+		mainTable.row();
+		mainTable.add(buttonsTable);
+	}
+
+	@Override
+	public void show()
+	{
+		Gdx.input.setInputProcessor(stage);
+	}
+
+	@Override
+	public void render(float delta)
+	{
+		Gdx.gl.glClearColor(AATinkererGame.BLUE.r, AATinkererGame.BLUE.g, AATinkererGame.BLUE.b, AATinkererGame.BLUE.a);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stage.act(delta);
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height)
+	{
+		camera.setToOrtho(false, width, height);
+		viewport.setWorldSize(width, height);
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void pause()
+	{
+
+	}
+
+	@Override
+	public void resume()
+	{
+
+	}
+
+	@Override
+	public void hide()
+	{
+		Gdx.input.setInputProcessor(null);
+	}
+
+	@Override
+	public void dispose()
+	{
+		stage.dispose();
+	}
+}
