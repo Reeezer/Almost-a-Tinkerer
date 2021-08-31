dc5ec502ae644063196f5fd35e8c42a80cc46212
Luca Davide Meyer <luca.meyer@he-arc.ch>
2021-08-31 11:54:25 +0200

    Tweaked difficulty

--- core/src/ch/hearc/p2/aatinkerer/data/Difficulty.java
+++ core/src/ch/hearc/p2/aatinkerer/data/Difficulty.java
@@ -2,10 +2,10 @@ package ch.hearc.p2.aatinkerer.data;
 
 public enum Difficulty
 {
-	REGULAR(100, 10, 0.6), //
-	ABUNDANT(400, 10, 0.6), //
-	RARE(10, 3, 0.6), //
-	BIGSPARSE(10, 30, 0.85), //
+	REGULAR(10, 10, 0.6), //
+	ABUNDANT(40, 10, 0.6), //
+	RARE(4, 3, 0.6), //
+	BIGSPARSE(2, 14, 0.75), //
 	EVERYWHERE(15000, 1, 0.1), //
 	GOODLUCK(1, 1, 0); //
 
